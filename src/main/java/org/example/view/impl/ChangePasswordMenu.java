1748f3204de082a715220c800541d8a41f839dec
timHa91 <tim.andre.hartmann@gmail.com>
2024-05-15 18:00:23 +0200

    added Settings functionality

--- src/main/java/org/example/view/impl/ChangePasswordMenu.java
+++ src/main/java/org/example/view/impl/ChangePasswordMenu.java
@@ -5,28 +5,29 @@ import org.example.view.Menu;
 
 import java.util.Scanner;
 
-public class ChangeEmailMenu implements Menu {
+public class ChangePasswordMenu implements Menu {
 
-    private final String EMAIL_CHANGED_SUCCESS = "Your email has been successfully changed";
+    private final String PASSWORD_CHANGED_SUCCESS = "Your password has been successfully changed";
     private final ApplicationContext applicationContext;
 
     {
+
         applicationContext = ApplicationContext.getInstance();
     }
     @Override
     public void start() {
         printMenuHeader();
         Scanner scanner = new Scanner(System.in);
-        String newEmail = scanner.next();
-        if (newEmail != null && !newEmail.isEmpty()) {
-            applicationContext.getLoggedInUser().setEmail(newEmail);
-            System.out.println(EMAIL_CHANGED_SUCCESS);
+        String newPassword = scanner.next();
+        if (newPassword != null && !newPassword.isEmpty()) {
+            applicationContext.getLoggedInUser().setPassword(newPassword);
+            System.out.println(PASSWORD_CHANGED_SUCCESS);
         }
     }
 
     @Override
     public void printMenuHeader() {
         System.out.println("***** CHANGE PASSWORD *****");
-        System.out.println("Please enter a new Email:");
+        System.out.println("Please enter a new Password:");
     }
 }
--- src/main/java/org/example/view/impl/SettingsMenu.java
+++ src/main/java/org/example/view/impl/SettingsMenu.java
@@ -1,15 +1,68 @@
 package org.example.view.impl;
 
+import org.example.configs.ApplicationContext;
 import org.example.view.Menu;
 
+import java.util.Scanner;
+
 public class SettingsMenu implements Menu {
+    private final String USER_INPUT_ERROR = "Only 1, 2 is allowed. Try one more time";
+    private final String NOT_LOGGED_IN_ERROR = "Please, log in or create new account to change your account settings";
+
+    private final ApplicationContext applicationContext;
+
+    {
+        applicationContext = ApplicationContext.getInstance();
+    }
     @Override
     public void start() {
+        if (applicationContext.getLoggedInUser() == null) {
+            System.out.println(NOT_LOGGED_IN_ERROR);
+            return;
+        }
 
+        Menu menuToNavigate;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            printMenuHeader();
+            String userInput = scanner.next();
+
+            if (userInput.equalsIgnoreCase(MainMenu.MENU_COMMAND)) {
+                menuToNavigate = applicationContext.getMainMenu();
+                break;
+            }
+
+            switch (userInput) {
+                case "1":
+                    menuToNavigate = new ChangePasswordMenu();
+                    break;
+                case "2":
+                    menuToNavigate = new ChangeEmailMenu();
+                    break;
+                case MainMenu.EXIT_COMMAND:
+                    System.out.println("Exiting the program.");
+                    return;
+                default:
+                    System.out.println(USER_INPUT_ERROR);
+                    continue;
+            }
+
+            if (menuToNavigate != null) {
+                break;
+            }
+        }
+        menuToNavigate.start();
     }
 
     @Override
     public void printMenuHeader() {
-
+        System.out.println("***** SETTINGS *****");
+        System.out.println(
+                "1. Change Password \n" +
+                "2. Change Email"
+                );
     }
+
+
 }
