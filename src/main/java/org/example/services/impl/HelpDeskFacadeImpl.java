c9e686dc9e1a874d91c4b59586eba1aaf30273bd
timHa91 <tim.andre.hartmann@gmail.com>
2024-05-24 22:09:57 +0200

    implemented the Support Ticket Comparator

--- src/main/java/org/example/services/impl/HelpDeskFacadeImpl.java
+++ src/main/java/org/example/services/impl/HelpDeskFacadeImpl.java
@@ -2,22 +2,45 @@ package org.example.services.impl;
 
 import org.example.models.SupportTicket;
 import org.example.services.HelpDeskFacade;
+import org.example.utils.comparators.CustomSupportTicketsComparator;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class HelpDeskFacadeImpl implements HelpDeskFacade {
 
+    public static HelpDeskFacade instance;
+    private final Queue<SupportTicket> supportTickets;
+
+    {
+        supportTickets = new PriorityQueue<>(new CustomSupportTicketsComparator());
+    }
+
+    public static HelpDeskFacade getInstance() {
+        if (instance == null) {
+            instance = new HelpDeskFacadeImpl();
+        }
+        return instance;
+    }
 
     @Override
     public void addNewSupportTicket(SupportTicket supportTicket) {
-
+        if(supportTicket == null) {
+            throw new IllegalArgumentException("Support Ticket can't be null");
+        }
+        boolean added = supportTickets.offer(supportTicket);
+        if (!added) {
+            throw new IllegalStateException("Unable to add support ticket to the queue");
+        }
     }
 
     @Override
     public SupportTicket getNextSupportTicket() {
-        return null;
+        return supportTickets.poll();
     }
 
     @Override
     public int getNumberOfTickets() {
-        return 0;
+        return supportTickets.size();
     }
 }
